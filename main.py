099aa583e5c818727c8f50bc2be0c65afdda0750
Igor Fidler <igorgfidler@hotmail.com>
2023-04-11 15:03:14 -0300

    Teste

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     env = envrmt.env('Simple_Env')
     drv1 = driver('drv1', [Belief('price',[10])], [Objective('offer')])
     drv2 = driver('drv2')
-    drv4 = driver('drv3', [Belief('price',[20])])
+    drv4 = driver('drv3', [Belief('price',[15])])
     mgr = manager('mgr')
     env.add_agents([drv1,drv2,drv4])
     env.add_agents(mgr)
