d39da1fea2454f6c63ca9f88e8913a93c7197631
alexandre <melladoallm@gmail.com>
2023-04-14 17:25:37 -0300

    First Ideas

---
+++ environment.py
@@ -0,0 +1,12 @@
+
+'''
+gerenciar 
+    caracteristicas do ambiente
+    artefato do ambiente
+    cargo de agentes neste ambiente
+    
+'''
+
+class env:
+    def __init__(self) -> None:
+        pass
