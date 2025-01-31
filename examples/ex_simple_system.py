4ae62e4db3b950f0a52a4acbeb5a804c3f82bb3f
Alexandre Mellado <mellado-27@outlook.com>
2025-01-31 14:09:30 -0300

    Add files via upload

--- examples/ex_simple_system.py
+++ examples/ex_simple_system.py
@@ -1,27 +1,25 @@
-from maspy import * 
+from maspy import *
 
 class SimpleEnv(Environment):
-    def env_act(self, agent1, agent2):
-        self.print(f"Contact between {agent1} and {agent2}")
+    def env_act(self, agt, agent2):
+        self.print(f"Contact between {agt} and {agent2}")
 
 class SimpleAgent(Agent):
-    @pl(gain,Goal("say_hello","Agent"))
+    @pl(gain,Goal("say_hello", Any))
     def send_hello(self,src,agent):
-        self.send(broadcast,tell,Belief("Hello",42),"SimpleChannel")
-        self.stop_cycle()
+        self.send(agent,tell,Belief("Hello"),"SimpleChannel")
 
-    @pl(gain,Belief("Hello","Value"))
-    def recieve_hello(self,src,value):
-        self.print(f"Hello and {value} received from {src}")
+    @pl(gain,Belief("Hello"))
+    def recieve_hello(self,src):
+        self.print(f"Hello received from {src}")
         self.env_act(src)
-        self.stop_cycle()
 
 if __name__ == "__main__":
+    Admin().set_logging(show_exec=True)
     agent1 = SimpleAgent()
     agent2 = SimpleAgent()
     env = SimpleEnv()
-    Change = Channel("SimpleChannel")
-    Admin().connect_to([agent1,agent2],[env,Change])
-    agent1.add(Goal("say_hello",(agent2.tuple_name,)))
+    ch = Channel("SimpleChannel")
+    Admin().connect_to([agent1,agent2],[env,ch])
+    agent1.add(Goal("say_hello",(agent2.my_name,)))
     Admin().start_system()
-    
