d6378acde27429e1cffdfa95dea190f37eaba908
Alexandre Mellado <mellado-27@outlook.com>
2025-02-02 20:07:36 -0300

    Add files via upload

--- examples/ex_simple_learn.py
+++ examples/ex_simple_learn.py
@@ -1,44 +1,43 @@
 from maspy import *
 from maspy.learning import *
-from time import sleep
+from random import choice
 
 class Map(Environment):
     def __init__(self, env_name=None):
         super().__init__(env_name)
-        
-        self.target = (2,2)
-        self.max_row = 4
-        self.max_col = 4
-        self.create(Percept("location", (self.max_row+1,self.max_col+1), cartesian))
-        self.possible_starts = {"location": [(0,0),(0,4),(4,0),(4,4)]}
+
+        self.targets = [(2,1),(1,2)]
+        self.max_row = 2
+        self.max_col = 3
+        size = (self.max_row+1,self.max_col+1)
+        self.create(Percept("location", size, cartesian))
+        self.possible_starts = {"location": [(0,0),(0,3)]}
     
     def move_transition(self, state: dict, direction: str):
         location = state["location"]
         
         location =self.moviment(location, direction)
-            
+
         state["location"] = location
         
-        if location == self.target:
-            reward = 100
+        if location in self.targets:
+            reward = 10
             terminated = True
         else:
             reward = -1
             terminated = False
         return state, reward, terminated
 
-    @action(listed, ["up","down","left","right"], move_transition)
+    @action(listed, ("up","down","left","right"), move_transition)
     def move(self, agt, direction: str):
-        self.print(f"{agt} is Moving {direction}")
         percept = self.get(Percept("location", (Any,Any)))
         assert isinstance(percept, Percept)
+        self.print(f"{agt} in {percept.args} is Moving {direction}")
         new_location = self.moviment(percept.args, direction)
         self.change(percept, new_location)
         self.print_percepts
-        if new_location == self.target:
-            self.print(f"{agt} reached the target")
-            Admin().stop_all_agents()
-        sleep(2)
+        if new_location in self.targets:
+            self.print(f"{agt} reached a target")
     
     def moviment(self, location, direction):
         if direction == "up" and location[0] > 0:
@@ -51,16 +50,42 @@ class Map(Environment):
             location = (location[0], location[1]+1)
         return location
     
-class Sample(Agent):
-    pass
-        
+class Mover(Agent):
+    def __init__(self, my_name=None):
+        super().__init__(my_name)
+        self.add(Goal("ask_for_guide"))
+
+    @pl(gain,Goal("ask_for_guide"))
+    def move_guide(self,src) -> None:
+        self.print("Asking for guide")
+        belief_model = self.send("Instructor", askOneReply, Belief("Model",("Map", Any)))
+        if isinstance(belief_model, Belief):
+            model: EnvModel = belief_model.args[1][0]
+            model.reset_percepts()
+            self.add_policy(model)
+            self.auto_action = True
+        else:
+            self.print("No model available for Map")
+            self.wait(3)
+            self.add(Goal("ask_for_guide"))
+
+class Instructor(Agent):
+    def __init__(self, my_name=None, env=None):
+        super().__init__(my_name)
+        self.add(Goal("make_model",[env]))
+    
+    @pl(gain,Goal("make_model",Any))
+    def make_model(self,src,env: list[Environment]):
+        self.print(f"Making model for {env[0].my_name}")
+        model = EnvModel(env[0])
+        print(f'actions: {model.actions_list}  space: {model.states_list}')
+        model.learn(qlearning, num_episodes=5000)
+        self.print(f"Finished training {env[0].my_name} Model")
+        self.add(Belief("Model",(env[0].my_name, [model])))
+            
 if __name__ == "__main__":
-    env = Map()
-    model = EnvModel(env)
-    print(f'actions: {model.action_space}  space: {model.observation_space}')
-    model.learn(qlearning)
-    ag = Sample()
-    ag.auto_action = True
-    ag.add_policy(model)
+    mv = Mover()
+    map = Map()
+    it = Instructor(env=map)
     Admin().start_system()
     
