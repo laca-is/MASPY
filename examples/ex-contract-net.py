625a260f57d466ecafd60d49eca201cc6eaa0516
Alexandre Mellado <mellado-27@outlook.com>
2025-09-26 20:17:22 -0300

    Update ex-contract-net.py

--- examples/ex-contract-net.py
+++ examples/ex-contract-net.py
@@ -5,11 +5,9 @@ class Initiator(Agent):
     def __init__(self, agt_name=None):
         super().__init__(agt_name)
         self.add(Goal("start"))
-        self.idle_counter = -1
 
     @pl(gain, Goal("start"))
     def start(self, src):
-        self.idle_counter = 0
         value = randint(15,30)
         self.add(Belief("half_value", value/2))
         self.print(f"Broadcasting value {value} to all participants")
@@ -17,7 +15,6 @@ class Initiator(Agent):
     
     @pl(gain, Goal("offer", Any), Belief("half_value", Any))
     def offer(self, src, offered_value, half_value):
-        self.idle_counter = 0
         if offered_value < half_value:
             self.print(f"{src}'s offer ({offered_value}) refused")
             self.send(src, tell, Belief("offer_refused", offered_value))
@@ -27,25 +24,21 @@ class Initiator(Agent):
     
     @pl(gain, Belief("negotiation_complete", Any))
     def negotiation_complete(self, src, value):
-        self.idle_counter = 0
         self.print(f"{src} accepted ({value}). Negotiation complete")
     
     @pl(gain, Belief("negotiation_failed", Any))
     def negotiation_failed(self, src, value):
-        self.idle_counter = 0
         self.print(f"{src} accepted ({value}) but had already negotiated")
 
 class Participant(Agent):
     def __init__(self, agt_name=None):
         super().__init__(agt_name)
-        self.idle_counter = -1
         self.best_offer = [500,""]
         self.add(Belief("max_value", randint(1,30)))
         self.add(Belief("Negotiating"))
     
     @pl(gain, Belief("request_offer", Any), [Belief("max_value", Any), Belief("Negotiating")])
     def request_offer(self, src, value, max_value):
-        self.idle_counter = 0
         if value > 1.5 * max_value:
             self.print(f"Refusing {src}'s value ({value}) > {max_value}")
         else:
@@ -64,11 +57,11 @@ class Participant(Agent):
     
     @pl(gain, Belief("offer_refused", Any))
     def offer_refused(self, src, value):
-        self.idle_counter = 0
         self.print(f"{src} rejected offer of {value}")
 
 if __name__ == "__main__":
     [Initiator() for _ in range(5)]
     [Participant() for _ in range(15)] 
     Admin().start_system()
+
 
